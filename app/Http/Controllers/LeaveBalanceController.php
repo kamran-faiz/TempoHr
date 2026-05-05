29a5c039830cf92bc92fefa0aeb95015928f67e7
kamran/faiz <faizkamran961@gmail.com>
2026-05-05 21:10:06 +0500

    feat(leave-balances): add backend controller and route

--- app/Http/Controllers/LeaveBalanceController.php
+++ app/Http/Controllers/LeaveBalanceController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Employee;
+use Inertia\Inertia;
 class LeaveBalanceController extends Controller
 {
-    //
+    public function index(){
+        $employee = Employee::with(['designation', 'department', 'leaveBalances.leaveType'])->latest()->get();
+        return Inertia::render('Leaves/LeaveBalances',[
+            'employees' => $employee,
+        ]);
+    }
 }
